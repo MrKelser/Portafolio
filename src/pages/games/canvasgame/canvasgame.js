dcb4e46c51294c12398a23704b8f6553ec30fc8c
MrKelser <proyectoskelser@gmail.com>
2023-02-02 19:36:20 -0300

    size

--- src/pages/games/canvasgame/canvasgame.js
+++ src/pages/games/canvasgame/canvasgame.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.height = window.innerHeight
-canvas.width = window.innerWidth
+canvas.height = screen.innerHeight
+canvas.width = screen.innerWidth
 
 class Player {
     constructor(x,y) {
