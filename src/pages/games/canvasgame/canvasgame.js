975fe5541d7a586f90ab62d50298736013b803ca
MrKelser <proyectoskelser@gmail.com>
2023-02-02 00:33:20 -0300

    jugador

--- src/pages/games/canvasgame/canvasgame.js
+++ src/pages/games/canvasgame/canvasgame.js
@@ -1,4 +1,16 @@
 const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
 canvas.height = innerHeight
 canvas.width = innerWidth
+
+class Player {
+    constructor(x,y,radio,color) {
+        this.x = x
+        this.y = y
+        this.radio = radio
+        this.color = color
+    }
+}
+
+const player = new Player(100, 100, 30, 'blue')
