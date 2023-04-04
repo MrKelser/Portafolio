d6a3a79e04cfeafefa97692ef620dcc6cf3fa76f
Mr Kelser <proyectoskelser@gmail.com>
2023-04-04 19:29:20 -0400

    size

--- src/pages/games/canvasgame/canvasgame.js
+++ src/pages/games/canvasgame/canvasgame.js
@@ -1,17 +1,18 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.height = screen.innerHeight
-canvas.width = screen.innerWidth
+canvas.height = innerHeight
+canvas.width = innerWidth
 
 class Player {
     constructor(x,y) {
-        this.x = x
-        this.y = y
 
         const pimg = new Image()
         pimg.src = 'img/Nave.png'
         this.image = pimg
+
+        this.x = x - this.image.width/2
+        this.y = y - this.image.height/2
     }
 
     draw() {
@@ -19,7 +20,7 @@ class Player {
     }
 }
 
-const player = new Player(canvas.width/2, canvas.height/2)
+const player = new Player(canvas.width/2 , canvas.height/2)
 player.draw()
 
 console.log(player);
